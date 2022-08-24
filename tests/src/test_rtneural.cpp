28629b9ba6a78ad9b2334b917d6f8a9407ace7d6
Massimo Pennazio <maxipenna@libero.it>
2022-08-24 09:19:48 +0200

    Added multi-model code to test

--- tests/src/test_rtneural.cpp
+++ tests/src/test_rtneural.cpp
@@ -4,7 +4,6 @@
 #include <math.h>
 
 #include <iostream>
-#include <variant>
 #include <RTNeural/RTNeural.h>
 
 #define JSON_MODEL_FILE_NAME "model.json"
@@ -12,11 +11,16 @@
 using namespace std;
 
 int main(void) {
-    RTNeural::ModelT<float, 3, 1,
-        RTNeural::DenseT<float, 3, 4>,
-        RTNeural::GRULayerT<float, 4, 8>,
-        RTNeural::DenseT<float, 8, 1>> model;
-
+    RTNeural::ModelT<float, 1, 1,
+        RTNeural::LSTMLayerT<float, 1, 16>,
+        RTNeural::DenseT<float, 16, 1>> lstm_16;
+    RTNeural::ModelT<float, 1, 1,
+        RTNeural::LSTMLayerT<float, 1, 12>,
+        RTNeural::DenseT<float, 12, 1>> lstm_12;
+    RTNeural::ModelT<float, 1, 1,
+        RTNeural::GRULayerT<float, 1, 8>,
+        RTNeural::DenseT<float, 8, 1>> gru_8;
+    int model_index = 0;
     int n_layers = 0;
     int input_size = 0;
     int hidden_size = 0;
@@ -34,8 +38,8 @@ int main(void) {
         std::ifstream jsonStream2(filePath);
         nlohmann::json modelData;
         jsonStream2 >> modelData;
-        model.parseJson(jsonStream, true);
 
+        /* Understand which model type to load */
         n_layers = modelData["layers"].size(); /* Get how many layers we have in this nn model */
 
         input_size = modelData["in_shape"].back().get<int>();
@@ -45,8 +49,8 @@ int main(void) {
         else
             in_skip = 0;
 
-        type = modelData["layers"][n_layers-1]["type"];
-        hidden_size = modelData["layers"][n_layers-1]["shape"].back().get<int>();
+        type = modelData["layers"][n_layers-1-1]["type"];
+        hidden_size = modelData["layers"][n_layers-1-1]["shape"].back().get<int>();
 
         std::cout << "input_size: " << input_size << std::endl;
         std::cout << "in_skip: " << in_skip << std::endl;
@@ -54,7 +58,37 @@ int main(void) {
         std::cout << "type: " << type << std::endl;
         std::cout << "hidden_size: " << hidden_size << std::endl;
 
-        std::cout << "Successfully loaded json file: " << filePath << std::endl;
+        if(type == std::string("lstm"))
+        {
+            if(hidden_size == 16)
+            {
+                model_index = 0;
+            }
+            else if(hidden_size == 12)
+            {
+                model_index = 1;
+            }
+        }
+        else if(type == std::string("gru"))
+        {
+            if(hidden_size == 8)
+            {
+                model_index = 2;
+            }
+        }
+
+        switch(model_index)
+        {
+            case 0:
+                lstm_16.parseJson(jsonStream, true);
+                break;
+            case 1:
+                lstm_12.parseJson(jsonStream, true);
+                break;
+            case 2:
+                gru_8.parseJson(jsonStream, true);
+                break;
+        }
     }
     catch (const std::exception& e) {
         std::cout << std::endl << "Unable to load json file: " << filePath << std::endl;
