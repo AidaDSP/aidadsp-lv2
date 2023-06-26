e34a850f725198e8ff01d59ede4877c6b232f7ba
Massimo Pennazio <maxipenna@libero.it>
2023-06-26 17:59:24 +0200

    We switch to trace for all this verbose stuff

--- rt-neural-generic/src/rt-neural-generic.cpp
+++ rt-neural-generic/src/rt-neural-generic.cpp
@@ -825,13 +825,13 @@ bool RtNeuralGeneric::testModel(LV2_Log_Logger* logger, DynamicModel *model, con
     }
     if(nErrs > 0)
     {
-        lv2_log_error(logger, "Failure %s: %d errors!\n", __func__, (int)nErrs);
-        lv2_log_error(logger, "Maximum error: %.12f, threshold: %.12f\n", max_error, threshold);
+        lv2_log_trace(logger, "Failure %s: %d errors!\n", __func__, (int)nErrs);
+        lv2_log_trace(logger, "Maximum error: %.12f, threshold: %.12f\n", max_error, threshold);
     }
     else
     {
-        lv2_log_note(logger, "Success %s: %d errors!\n", __func__, (int)nErrs);
-        lv2_log_note(logger, "Maximum error: %.12f, threshold: %.12f\n", max_error, threshold);
+        lv2_log_trace(logger, "Success %s: %d errors!\n", __func__, (int)nErrs);
+        lv2_log_trace(logger, "Maximum error: %.12f, threshold: %.12f\n", max_error, threshold);
         return true;
     }
     return false;
